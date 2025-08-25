faba0e09573a010ee923132c2c74a3b6aa498440
ym12122929-sketch <ym12122929@gmail.com>
2025-08-25 15:32:51 +0000

    container config done

---
+++ .devcontainer/01-basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "mounu1229@gmail.com"
+var accouuntPassword = "1234"
+accountCity = "PATNA"
+
+// accountId = 2 // not allowed
+accountEmail = "hc@hc.com"
+accouuntPassword = "4321"
+accountCity = "dehli"
+lat accountState;
+console.log(accountId);
+/*
+Prefer not to use varn
+beacause of issue in block scope and functional scope
+console.table([ accountId, accountEmail, accouuntPassword, accountCity, accountState]);
